00b99894a5656bc2aabba74242b2ff9a45ab0750
Tasin Sayed <sayet025.309@gmail.com>
2024-05-22 01:23:57 +0100

    Addedd all loading scenarios - output matrices computed

--- Sim_Geo.py
+++ Sim_Geo.py
@@ -2,16 +2,23 @@ import numpy as np
 from numpy.linalg import inv
 
 #Grip-limited scenario
-F_grip = []
-#Tilt telt scenario
-F_tilt = [0, 3031, 1750, 0]
+F_grip = [0, 5770/2, 3500/2, 0]
+#Tilt telt scenario - half load on one side of wheels
+F_tilt = [0, 3031/2, 1750/2, 0]
 #Dive - hard braking
-F_dive = []
-#Bump hit
-F_bump = []
+F_dive = [0,0, 3500/2, -6210/2]
+#Braking_bump hit
+F_brake_bump = [-2*1750, 0 , 1750+5292, -6210/2]
+#Grip limited bump
+F_corner_bump = [-2*1750, 5770/2 , 1750+5292, 0]
+#Bump (solo)
+F_bump = [-1764, 0, 882+2646, 0]
 
 
-#Define Suspension dimensions
+#F1x, F0y, F0z, F0x  = forces_external matrix
+#Must set F1x to 0 - as this is an internal load
+
+#Define Suspension geometry dimensions
 r = 266.7
 a = 120
 b = 110
@@ -20,7 +27,7 @@ d = 77.48
 
 #External reaction forces - through rigid wishbone + hib + rim/tire body
 
-#*Reaction_Fsus = [F2y, F3y, F4y, F3x, F4x, F4z] #6 variable unknown function - use for mapping forces from "resolve" function output
+#*Reaction_Fsus = [F2y, F3y, F4y, F3x, F4x, F4z] #6 variable unknown function - use for mapping forces from "resolve" function output      
 
 F_A = ([0,0,0, (r+a)/r,(r-b)/r,0],
 [c ,0 ,0 , d, d, 0],
@@ -38,16 +45,17 @@ def resolve(forces_external, F_Ainv):
     F0z = forces_external[2]
     F0x = forces_external[3]
     #Capture external loads for varying driving situations
-    F_ext = [-1*F1x, 0, -1*F0y, -1*F0z, -F0x, 0]
+    F_ext = [-1*F1x, 0, -1*F0y, -1*F0z, -1*F0x, 0]
     Reaction_Fsus  = np.dot(F_Ainv, F_ext)
     return Reaction_Fsus
 
-load_models = {"brake_dive": F_dive, "tilt_test": F_tilt , "max_lateral": F_grip, "Bump_load": F_bump}
+load_models = {"brake_dive": F_dive, "tilt_test": F_tilt , "max_lateral": F_grip, "Bump_when_braking": F_brake_bump, "Bump_when_cornering": F_corner_bump, "F_bump": F_bump}
 #Test_case with trivial lateral load transfer and tilt test case
 test_load_models = {"tilt": F_tilt}
 
-for key in test_load_models:
-    print("Load Case:{}, Forces Matrix: {}".format(key, resolve(test_load_models[key], F_Ainv)))
+
+for key in load_models:
+    print("\nLoad Case:{}, Forces Matrix: {}".format(key, resolve(load_models[key], F_Ainv)))
     #Captures the matrix of F1x, F0x, F0z, F0y matrix inversion is applied to resolve for unknown upright rigid body loads
 
     
